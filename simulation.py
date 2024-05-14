6a594d79fc4249e85cbdabc5e0d7ce59d6e98369
Philipp Dijkstal <philipp.dijkstal@psi.ch>
2024-05-14 11:23:33 +0200

    add function that returns the k1ls

--- simulation.py
+++ simulation.py
@@ -145,4 +145,52 @@ class ElegantSimulation:
 
         return np.sqrt(x*xp - xxp**2)
 
+    def get_k1ls(self, combine_q1q2=True, remove_mqup=True):
+        quad_indices = self.par['ElementType'] == 'QUAD'
+        k1_indices = self.par['ElementParameter'] == 'K1'
+        length_indices = self.par['ElementParameter'] == 'L'
+        quad_names = np.unique(self.par['ElementName'][quad_indices])
+        k1 = np.zeros_like(quad_names, dtype=float)
+        length = k1.copy()
+        for ctr, quad_name in enumerate(quad_names):
+            k1_index = np.logical_and(self.par['ElementName'] == quad_name, k1_indices)
+            assert k1_index.sum() == 1
+            k1[ctr] = self.par['ParameterValue'][k1_index]
+
+            length_index = np.logical_and(self.par['ElementName'] == quad_name, length_indices)
+            assert length_index.sum() == 1
+            length[ctr] = self.par['ParameterValue'][length_index]
+
+        if remove_mqup:
+            indices = np.array(['MQUP' in x for x in quad_names], bool)
+            quad_names = quad_names[~indices]
+            k1 = k1[~indices]
+            length = length[~indices]
+
+        if combine_q1q2:
+            quad_names0 = quad_names
+            k10 = k1
+            length0 = length
+            quad_names, k1, length = [], [], []
+            for quad_name, this_k1, this_length in zip(quad_names0, k10, length0):
+                if quad_name.endswith('.Q1'):
+                    quad_names.append(quad_name[:-3])
+                    k1.append(this_k1)
+                    length.append(this_length*2)
+                elif quad_name.endswith('.Q2'):
+                    pass
+                else:
+                    quad_names.append(quad_name)
+                    k1.append(this_k1)
+                    length.append(this_length)
+            quad_names = np.array(quad_names)
+            k1 = np.array(k1)
+            length = np.array(length)
+
+        return {
+                'quad_names': quad_names,
+                'k1': k1,
+                'length': length,
+                'k1l': k1*length,
+                }
 
